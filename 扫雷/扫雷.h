c22839c0760a8515310de7f4588c114c6dbefbf5
wanshouxujiu <97508633+wanshouxujiu@users.noreply.github.com>
2022-01-24 11:12:13 +0800

    Update 扫雷.h

--- 扫雷/扫雷.h
+++ 扫雷/扫雷.h
@@ -12,4 +12,4 @@ void Initboard(char board[ROWS][COLS], int rows, int cols, char set);
 void DisplayBoard(char board[ROWS][COLS], int row, int col);
 void SetMine(char board[ROWS][COLS], int row, int col);
 void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
-int get_mine_count(char mine[ROWS][COL], int row, int col);
+int get_mine_count(char mine[ROWS][COLS], int row, int col);
